32589723f88f4d4ecb51d8424ac1d0a7b352864d
vghost <bluetornado@zju.edu.cn>
2019-05-28 10:14:45 +0800

    Fix a bug of typo.

--- GN/graph.py
+++ GN/graph.py
@@ -291,10 +291,11 @@ class DynamicAdjacentMatrix:
 
     def avg_edges_data_for_point(self,i):
         s_edges_indexs,r_edges_indexs = self.points_to_edges_index[i]
-        default_value_s = tf.get_variable("default_edge_s",shape=[1,self.edges_data.get_shape().as_list()[-1]],
+        with tf.variable_scope("default_edge_value", reuse=tf.AUTO_REUSE):
+            default_value_s = tf.get_variable("default_edge_s",shape=[1,self.edges_data.get_shape().as_list()[-1]],
                                           dtype=tf.float32,
                                           initializer=tf.zeros_initializer,trainable=True)
-        default_value_r = tf.get_variable("default_edge_r",shape=[1,self.edges_data.get_shape().as_list()[-1]],
+            default_value_r = tf.get_variable("default_edge_r",shape=[1,self.edges_data.get_shape().as_list()[-1]],
                                           dtype=tf.float32,
                                           initializer=tf.zeros_initializer,trainable=True)
         s_edge = DynamicAdjacentMatrix.safe_gather(self.edges_data,s_edges_indexs,default_value=default_value_s)
